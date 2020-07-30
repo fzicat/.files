c49a11516fd8ef743d1b69aa4749337e3d4d2b2c
fzicat <fzicat@bzinc.ca>
2020-07-29 21:32:19 -0400

    add init.vim for neovim

--- setup.sh
+++ setup.sh
@@ -10,11 +10,13 @@ echo ".files"
 cd $HOME
 mkdir -p $HOME/.vim/swapfiles
 mkdir -p $HOME/.vim/config
+mkdir -p $HOME/.config/nvim
 
 echo "Linking dotfile..."
 ln -fs .files/.bashrc_custom
 ln -fs .files/.tmux.conf
 ln -fs .files/.vimrc
 ln -fs $HOME/.files/coc.vim $HOME/.vim/config/coc.vim
+ln -fs $HOME/.files/init.vim $HOME/.config/nvim/init.vim
 
 echo "Done!"
