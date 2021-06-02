61cef380d0ca254c12e9678971c3e29d33abe8bd
fzicat <fzicat@bzinc.ca>
2021-06-02 15:53:17 -0400

    dev

--- setup.sh
+++ setup.sh
@@ -10,21 +10,30 @@
 
 echo ".files"
 
-cd $HOME
-mkdir -p $HOME/.vim/swapfiles
-mkdir -p $HOME/.vim/config
-mkdir -p $HOME/.config/nvim
+#cd $HOME
+#mkdir -p $HOME/.vim/swapfiles
+#mkdir -p $HOME/.vim/config
+#mkdir -p $HOME/.config/nvim
 
-echo "Linking dotfile..."
-ln -fs .files/.bashrc_custom
-ln -fs .files/.tmux.conf
-ln -fs .files/.vimrc
-ln -fs $HOME/.files/init.vim $HOME/.config/nvim/init.vim
-ln -fs $HOME/.files/coc.vim $HOME/.vim/config/coc.vim
-ln -fs $HOME/.files/startify.vim $HOME/.vim/config/startify.vim
-ln -fs $HOME/.files/lightline.vim $HOME/.vim/config/lightline.vim
-ln -fs $HOME/.files/pudb.vim $HOME/.vim/config/pudb.vim
-ln -fs $HOME/.files/powerline_fz.vim $HOME/.vim/plugged/lightline.vim/autoload/lightline/colorscheme/powerline_fz.vim
-ln -fs $HOME/.files/dwm/config.def.h $HOME/dwm-6.2/config.def.h
+#echo "Linking dotfile..."
+#ln -fs .files/.bashrc_custom
+#ln -fs .files/.tmux.conf
+#ln -fs .files/.vimrc
+#ln -fs $HOME/.files/init.vim $HOME/.config/nvim/init.vim
+#ln -fs $HOME/.files/coc.vim $HOME/.vim/config/coc.vim
+#ln -fs $HOME/.files/startify.vim $HOME/.vim/config/startify.vim
+#ln -fs $HOME/.files/lightline.vim $HOME/.vim/config/lightline.vim
+#ln -fs $HOME/.files/pudb.vim $HOME/.vim/config/pudb.vim
+#ln -fs $HOME/.files/powerline_fz.vim $HOME/.vim/plugged/lightline.vim/autoload/lightline/colorscheme/powerline_fz.vim
+#ln -fs $HOME/.files/dwm/config.def.h $HOME/dwm-6.2/config.def.h
+
+ln -fs $HOME/.files/shell $HOME/.config/shell
+ln -fs $HOME/.files/zsh $HOME/.config/zsh
+ln -fs $HOME/.files/lf $HOME/.config/lf
+ln -fs $HOME/.files/tmux/tmux.conf $HOME/.tmux.conf
+ln -fs $HOME/.files/nvim/init.vim $HOME/.config/nvim/init.vim
+ln -fs $HOME/.files/nvim/config $HOME/.config/nvim/config
+
+ln -fs $HOME/.config/shell/profile $HOME/.zprofile
 
 echo "Done!"
